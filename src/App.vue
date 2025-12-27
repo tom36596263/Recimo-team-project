956cf580a6d5b8bc47624ea0876e8a6fe06865f9
tom36596263 <tom36596263@gmail.com>
2025-12-27 14:09:40 +0800

    新增頁面

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/timothyTest">陳彤的頁面</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/timothyTestView.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <div>陳彤的頁面</div>
+</template>
+
+<style lang="scss" scoped>
+    div{
+        font-size: 3rem;
+    }
+</style>
