375d39a2ee83f8557a4d6d67a72c5b0176af96ed
Helen-Kuo <helenkuo85@gmail.com>
2025-12-27 14:07:46 +0800

    首頁router

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/index">首頁</RouterLink>
       </nav>
     </div>
   </header>
